c9e6c95a1963cfc450b37fe5addffec72f5fb7be
Messias-Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-08-24 21:24:18 -0300

    fix: change main.py to the correct version && fix: change calculate function result to the correct form

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-import numpy as np
+# This entrypoint file to be used in development. Start by reading README.md
+import mean_var_std
+from unittest import main
 
-def calculate(list):
-    #Verificar se tem nove números
-    if len(list) != 9:
-        raise ValueError("List must contain nine list.")
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
-    #Transformar em uma mtriz 3x3 
-    array = np.array(list).reshape(3,3)
-
-    #Percorrer o array de funções e criar o dicionário
-    methods = ["mean", "var", "std", "max", "min", "sum"]
-
-    calculations = {}
-    for name in methods:
-        calculations[name] = [getattr(np,name)(array, axis=0).tolist(), getattr(np,name)(array, axis=1).tolist(), getattr(np,name)(array).tolist()]   
-
-    return calculations
+# Run unit tests automatically
+main(module='test_module', exit=False)
